4aaf9e5bb630f75c290b707aa9873fbad67aaa48
Dima Chavro <dchavro@gmail.com>
2022-07-03 16:13:01 +0100

    Intialise assoications between Events and Users

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+
+  has_many :attendance_records, foreign_key: 'attended_event_id'
+  has_many :attendees, through: :attendance_records, source: :event_attendee
 end
