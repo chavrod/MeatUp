c6778493333deed452afc9ac386af5137ed4533d
Dima Chavro <dchavro@gmail.com>
2022-07-04 07:15:59 +0100

    Fix association typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
 
-  has_many :attendance_records, foreign_key: :attendee_id
+  has_many :attendance_records, foreign_key: :event_attendee_id
   has_many :attended_events, through: :attendance_records
 end
