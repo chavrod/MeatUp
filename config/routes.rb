31c580b924f2bd354b97406019f6536252054802
Dima Chavro <dchavro@gmail.com>
2022-07-04 19:24:06 +0100

    Add link to user profile and a temporary boilerplate profile view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :events do
     resources :attendance_records, only: %i[create destroy]
   end
-  
+
+  resources :users, only: %i[show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "events#index"
