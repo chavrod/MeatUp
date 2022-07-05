21b27460dd5a0d3ee409a0cc1fb0d2a4d6304a98
Dima Chavro <dchavro@gmail.com>
2022-07-05 08:39:37 +0100

    Create scope for upcoming/past events and render in Index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  scope :past, -> { where('date <= ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
+
   belongs_to :creator, class_name: 'User'
 
   has_many :attendance_records, foreign_key: :attended_event_id
